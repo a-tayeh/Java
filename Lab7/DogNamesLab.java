6d13702ea4956a03d21495fd99baf3319d828235
AliTayeh <tayehas@vcu.edu>
2018-02-26 23:18:20 -0500

    add which-dog-is-famous game

--- Lab7/DogNamesLab.java
+++ Lab7/DogNamesLab.java
@@ -1,10 +1,12 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Math;
 public class DogNamesLab {
     private static ArrayList<Dog>  dogsArray = new ArrayList<Dog>();
     private static Dog dog = new Dog();
     public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
 //        ArrayList<String> me = new ArrayList<String>();
 //        me.add("ali");
 //        me.add("cally");
@@ -21,9 +23,9 @@ public class DogNamesLab {
         int count = 0;
         try {
             Scanner in = new Scanner(new File("Dog_Names.csv"));
-
+            //gets rid of first useless row
             firstLine = in.nextLine();
-            while(in.hasNextLine() && !in.nextLine().equals("")){
+            while(in.hasNextLine() && !(in.nextLine().equals(""))){
                 dogData = in.nextLine().split(",");
                 dogName = dogData[0];
                 count = Integer.parseInt(dogData[1]);
@@ -47,7 +49,7 @@ public class DogNamesLab {
             System.out.println(e.getMessage());
         }
         if(args.length>0) {
-            Scanner input = new Scanner(System.in);
+
             if(args[0].equals("1")) {
                 System.out.print("Enter your dog's name:");
                 dogName = input.nextLine();
@@ -63,7 +65,55 @@ public class DogNamesLab {
                     System.out.println(a.getDogName());
                 }
             }
+            else if(args[0].equals("3")) {
+                boolean playAgain = false;
+
+                while (!playAgain) {
+
+                    int choice = 0;
+                    int randDog1 = (int) Math.ceil(Math.random() * dogsArray.size());
+                    int randDog2 = (int) Math.ceil(Math.random() * dogsArray.size());
+                    int randDog1Count = dogsArray.get(randDog1).getCount();
+                    int randDog2Count = dogsArray.get(randDog2).getCount();
+                    String randDog1Name = dogsArray.get(randDog1).getDogName();
+                    String randDog2Name = dogsArray.get(randDog2).getDogName();
+                    System.out.println("Which name is more popular for Anchorage dogs?(Type 1 or 2)");
+                    System.out.printf("1. %s %7s2. %s\n", randDog1Name, "", randDog2Name);
+                    choice = input.nextInt();
+
+                    if (randDog1Count > randDog2Count && choice == 1) {
+                        System.out.printf("Correcto mundo, %s has a count of %d while %s has a " +
+                                "count of %d", randDog1Name, randDog1Count, randDog2Name, randDog2Count);
+                    }
+                    if (randDog1Count < randDog2Count && choice == 1) {
+                        System.out.printf("Nope, %s has a count of %d while %s has a " +
+                                "count of %d", randDog1Name, randDog1Count, randDog2Name, randDog2Count);
+                    }
+                    if (randDog1Count < randDog2Count && choice == 2) {
+                        System.out.printf("Correcto mundo, %s has a count of %d while %s has a " +
+                                "count of %d", randDog1Name, randDog1Count, randDog2Name, randDog2Count);
+                    }
+                    if (randDog1Count > randDog2Count && choice == 2) {
+                        System.out.printf("Nope, %s has a count of %d while %s has a " +
+                                "count of %d", randDog1Name, randDog1Count, randDog2Name, randDog2Count);
+                    }
+                    System.out.println("Wanna Play Again? Y/N");
+                    String reset = input.nextLine();
+
+                    if (reset.equalsIgnoreCase("Y")) {
+                        playAgain = false;
+                    } else if (reset.equalsIgnoreCase("N")) {
+                        playAgain = true;
+                    }
+               }
+            }
         }
+
+
+
+
+
+
     }
 
     public static int searchDogName(String dogName){

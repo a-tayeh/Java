33f732a5d9d353a963f0db5d461919ca9e40f547
AliTayeh <tayehas@vcu.edu>
2018-03-01 16:53:54 -0500

    fix countersArray-size bug

--- Project_3/Sorter.java
+++ Project_3/Sorter.java
@@ -2,23 +2,44 @@ import java.util.ArrayList;
 
 public class Sorter{
     public static void main(String [] args) {
-//        ArrayList<int[]> coll = new ArrayList<int[]>();
-        int[] nums = {4, 4, 3, 1, 2, 3,0};
-        int [] counters = new int[nums.length];
+
+        int[] nums = {4, 4, 3, 1, 2, 3,0,17,17,17};
+        int largest = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(largest<nums[i]){
+                largest = nums[i];
+            }
+        }
+        int [] counters = new int[largest+1];
 
         for (int i = 0; i < nums.length; i++){
             counters[nums[i]]++;
         }
         int index = 0;
-//        System.out.println("There are:");
-        for(int i = 0;i<counters.length;i++){
+        for(int i = 0;i<largest+1;i++){
             if(counters[i]!=0) {
-                System.out.println("There are: " + counters[i] + " --> " + index);
+                System.out.println("There are: " + counters[i] + " ---> " + index);
+                index++;
+            }
+            else{
                 index++;
             }
         }
 
-        int [] sorted = new int[nums.length];
+//        int [] sorted = new int[nums.length];
+//        int index2 = 0;
+//        while(index2<nums.length){
+//            for(int i = 0;i<counters.length;i++){
+//                sorted[i] = i;
+//            }
+//            index2++;
+//        }
+//
+//        for(int a : sorted){
+////            if(a!=0) {
+//                System.out.print(a + " ");
+////            }
+//        }
 
 
     }

d434c6cde8ddfe4cb0fc733e6679dada469392dd
AliTayeh <tayehas@vcu.edu>
2018-03-04 11:49:40 -0500

    refactor Sorter class and ad junit test class for Sorter

--- Project_3/Sorter.java
+++ Project_3/Sorter.java
@@ -1,77 +1,35 @@
-import java.util.ArrayList;
-
 public class Sorter{
-    public static void main(String [] args) {
 
-        int[] nums = {4, 4, 3, 1, 2, 3,0,17,17,17};
-        int [] sorted = new int [nums.length];
-        int largest = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(largest<nums[i]){
-                largest = nums[i];
-            }
+    public static int[] sortIntArray(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]<0){ throw new IllegalArgumentException("The array cannot contain negative integers"); }
         }
+        int largest = 0;
+
+        for(int i = 0;i<arr.length;i++){
+            if(largest<arr[i]) { largest = arr[i]; }
+        }
+
         int [] counters = new int[largest+1];
 
-        for (int i = 0; i < nums.length; i++){
-            counters[nums[i]]++;
-        }
-        ArrayList<Integer> met = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) { counters[arr[i]]++ ; }
+
         int index = 0;
-        int index2 = 0;
-        boolean sort = false;
-
-
-            for(int i = 0;i<counters.length;i++){
-            if(counters[i]!=0) {
-
-                System.out.println("There are: " + counters[i] + " ---> " + index);
-                met.add(counters[i]);
-
-                index++;
-
-
-            }
-            else{
-
-                index++;
-
+        for(int i = 0;i<counters.length;i++) {
+            if (counters[i] != 0){
+                int iterationTimes = 0;
+                while (iterationTimes < counters[i]) {
+                    if (iterationTimes > 0) {
+                        arr[index] = i;
+                        iterationTimes++;
+                    } else {
+                        arr[index] = i;
+                        iterationTimes++;
+                    }
+                    index++;
+                }
             }
         }
-        int [] newCounters = new int[met.size()];
-            for(int i = 0;i<newCounters.length;i++){
-                newCounters[i] = met.get(i);
-            }
-          int index8 = 0;
-          for(int i = 0;i<18;i++) {
-              if (counters[i] != 0){
-                  int index3 = 0;
-                  while (index3 < counters[i]) {
-                      if (index3 > 0) {
-                          nums[index8] = i;
-                          index3++;
-                      } else {
-                          nums[index8] = i;
-                          index3++;
-                      }
-                      index8++;
-                  }
-          }
-          }
-
-        for(int a : nums){
-            System.out.print(a+" ");
-        }
-
-
-
-
-
-
+        return arr;
     }
-
-//    public int[] sortIntArray(int[] arr){
-//
-//        return arr;
-//    }
 }
---
+++ Project_3/SorterTest.java
@@ -0,0 +1,72 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SorterTest {
+
+    @Test
+    public void sortIntArray1() {
+        int[] arr = {1,2,2,55,55,11,1,3,2};
+        int [] expected = {1,1,2,2,2,3,11,55,55};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void sortIntArray2() {
+        int[] arr = {0,0,0,0,0,0};
+        int [] expected = {0,0,0,0,0,0};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void sortIntArray3() {
+        int[] arr = {0,0,0,1,0,0,0};
+        int [] expected = {0,0,0,0,0,0,1};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void sortIntArray4() {
+
+        int[] arr = {0,0,0,1,-1,0,0};
+        int [] expected = {0,0,0,0,0,0,1};
+         Sorter.sortIntArray(arr);
+    }
+
+    @Test
+    public void sortIntArray5() {
+        int[] arr = {0};
+        int [] expected = {0};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void sortIntArray6() {
+        int[] arr = {-1,-1,-1,-1};
+         Sorter.sortIntArray(arr);
+    }
+    @Test
+    public void sortIntArray7() {
+        int[] arr = {1,2,3,4,5};
+        int [] expected = {1,2,3,4,5};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void sortIntArray8() {
+        int[] arr = {9,9,8,8,7,6,54,4,3,2,0,0};
+        int [] expected = {0,0,2,3,4,6,7,8,8,9,9,54};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void sortIntArray9() {
+        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        int [] expected = {1,2,3,4,5,6,7,8,9,10};
+        int [] actual = Sorter.sortIntArray(arr);
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+}

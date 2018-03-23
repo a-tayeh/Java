6d98ee910482ee930e737e9e0571ab3db58cb113
AliTayeh <tayehas@vcu.edu>
2018-03-23 19:28:06 -0400

    tweak matchedTags to recognize data on same line or if it's on different line

--- Project_4/SongReader.java
+++ Project_4/SongReader.java
@@ -8,6 +8,8 @@ public class SongReader {
 
     public static boolean matchedTags(String fileName) {
         StackInterface<String> tagStack = new ArrayStack<>();
+        String temp = "";
+        String tag = "";
 
         try {
             Scanner reader = new Scanner(new File(fileName));
@@ -20,19 +22,45 @@ public class SongReader {
                     if (k == -1) {
                         return false;
                     }
-                    String tag = line.substring(j, k + 1);
+                     tag = line.substring(j, k + 1);
+
                     if (!tag.startsWith("</")) {
                         tagStack.push(tag);
                     } else {
-                        String temp = tag.replace("/", "");
+                        temp = tag.replace("/", "");
+                        if(line.contains(tag) && line.contains(temp)) {
+                            String data = line.replaceAll(tag, "");
+                            data = data.replaceAll(temp, "");
+                            System.out.println(temp.replaceAll("<|>","")+": "+data.trim());
+                        }
 
                         if (temp.equals(tagStack.peek())) {
                             tagStack.pop();
+
+//                            System.out.println(data);
                         }
 
                     }
                     j = line.indexOf('<', k + 1);
+
+
                 }
+
+                String data = line.replaceAll(tag,"");
+                data = data.replaceAll(temp,"");
+                if(data.length()>0){
+                    data = data.trim();
+                }
+                if(tag.equals("<artist>") && !data.equals("")){
+                    System.out.println("artist: "+data);
+                }
+                else if(tag.equals("<album>")&&!data.equals("")){
+                    System.out.println("albums: "+data);
+                }
+                else if(tag.equals("<title>")&&!data.equals("")){
+                    System.out.println("title: "+data);
+                }
+
         }
     }catch (Exception e){
             System.out.println(e.getMessage());
